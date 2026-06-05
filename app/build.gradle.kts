e848929ca1360830b99f1bbe1b66e5e58fd12511
Enderthor <58392928+lockevod@users.noreply.github.com>
2026-06-05 14:24:20 +0200

    test: añadir JUnit e infraestructura de tests unitarios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,5 +61,6 @@ dependencies {
     implementation(libs.timber)
     implementation(libs.androidx.foundation.android)
     implementation(libs.androidx.foundation.layout.android)
+    testImplementation(libs.junit)
 }
 
